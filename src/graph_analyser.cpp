e6b1b99d2710d215b592c0159173a335cf1c2b49
Tom Grant <thomas.grant.duck321@gmail.com>
2025-04-16 21:36:05 +0100

    JIT: reduce analysis time-complexity
    Callgrind believes this cuts off ~30% from the regex compile for graphs
    with many nodes but not many cross-connections.

--- src/graph_analyser.cpp
+++ src/graph_analyser.cpp
@@ -1,68 +1,117 @@
 #include "private/graph_analyser.hpp"
 #include "private/nfa.hpp"
 
-#include <cstddef>
+#include <cstdint>
 #include <queue>
+#include <ranges>
 
 using namespace uregex;
 
+struct NodeIndex {
+  uint32_t value;
+};
+struct CounterIndex {
+  uint32_t value;
+};
+
+struct StartGroupIndex {
+  uint32_t value;
+};
+
+struct EndGroupIndex {
+  uint32_t value;
+};
+
 GraphAnalyzer::GraphAnalyzer(RegexGraphImpl const &graph)
     : m_node_count{graph.all_nodes.size()},
-      m_distance_map(m_node_count * m_node_count),
       m_non_zero_counter_map(m_node_count),
       m_maybe_start_group_map(m_node_count),
       m_maybe_end_group_map(m_node_count) {
-  std::fill(m_distance_map.begin(), m_distance_map.end(), ~0UL);
+  std::queue<std::pair<NodeIndex, CounterIndex>> remaining_counters;
+  std::queue<std::pair<NodeIndex, StartGroupIndex>> remaining_start_groups;
+  std::queue<std::pair<NodeIndex, EndGroupIndex>> remaining_end_groups;
 
-  for (size_t start_node = 0; start_node < m_node_count; start_node += 1) {
-    std::queue<std::pair<uint32_t, uint32_t>> remaining;
-    remaining.emplace(1, start_node);
-
-    while (not remaining.empty()) {
-      auto [generation, current_node] = remaining.front();
-      remaining.pop();
-
-      for (auto const &edge : graph.all_nodes[current_node]->edges) {
-        if (not are_connected(edge.output_index, start_node)) {
-          remaining.emplace(generation + 1, edge.output_index);
-          distance(edge.output_index, start_node) = generation;
-        }
+  for (auto const &node : graph.all_nodes) {
+    for (auto const &edge : node->edges) {
+      for (auto counter : edge.counters) {
+        remaining_counters.emplace(edge.output_index, counter);
+        m_non_zero_counter_map[edge.output_index].insert(counter);
+      }
+      for (auto start_group : edge.start_groups) {
+        remaining_start_groups.emplace(edge.output_index, start_group);
+        m_maybe_start_group_map[edge.output_index].insert(start_group);
+      }
+      for (auto end_group : edge.end_groups) {
+        remaining_end_groups.emplace(edge.output_index, end_group);
+        m_maybe_end_group_map[edge.output_index].insert(end_group);
       }
     }
   }
 
-  for (auto const &node : graph.all_nodes) {
-    for (auto const &edge : node->edges) {
-      for (size_t to_node = 0; to_node < m_node_count; to_node += 1) {
-        if (not are_connected(to_node, edge.output_index) &&
-            to_node != edge.output_index) {
-          continue;
-        }
+  while (not remaining_counters.empty()) {
+    auto [node_index, counter_index] = remaining_counters.front();
+    remaining_counters.pop();
 
-        for (size_t counter : edge.counters) {
-          m_non_zero_counter_map[to_node].insert(counter);
-        }
-        for (size_t group : edge.start_groups) {
-          m_maybe_start_group_map[to_node].insert(group);
-        }
-        for (size_t group : edge.end_groups) {
-          m_maybe_end_group_map[to_node].insert(group);
-        }
+    for (auto const &edge : graph.all_nodes[node_index.value]->edges) {
+      if (not m_non_zero_counter_map[edge.output_index].contains(
+              counter_index.value)) {
+        m_non_zero_counter_map[edge.output_index].insert(counter_index.value);
+        remaining_counters.emplace(edge.output_index, counter_index.value);
+      }
+    }
+  }
+
+  while (not remaining_start_groups.empty()) {
+    auto [node_index, start_group_index] = remaining_start_groups.front();
+    remaining_start_groups.pop();
+
+    for (auto const &edge : graph.all_nodes[node_index.value]->edges) {
+      if (not m_maybe_start_group_map[edge.output_index].contains(
+              start_group_index.value)) {
+        m_maybe_start_group_map[edge.output_index].insert(
+            start_group_index.value);
+        remaining_start_groups.emplace(edge.output_index,
+                                       start_group_index.value);
+      }
+    }
+  }
+
+  while (not remaining_end_groups.empty()) {
+    auto [node_index, end_group_index] = remaining_end_groups.front();
+    remaining_end_groups.pop();
+
+    for (auto const &edge : graph.all_nodes[node_index.value]->edges) {
+      if (not m_maybe_end_group_map[edge.output_index].contains(
+              end_group_index.value)) {
+        m_maybe_end_group_map[edge.output_index].insert(end_group_index.value);
+        remaining_end_groups.emplace(edge.output_index, end_group_index.value);
       }
     }
   }
 }
 
 auto GraphAnalyzer::visualize(std::ostream &output) const -> void {
-  for (size_t n1 = 0; n1 < m_node_count; n1 += 1) {
-    for (size_t n2 = 0; n2 < m_node_count; n2 += 1) {
-      auto d = distance(n1, n2);
-      if (d != ~0UL) {
-        output << std::format("{:<5}", d);
-      } else {
-        output << std::format("{:<5}", "x");
-      }
+  output << "Non-zero counters:";
+  for (auto const &[index, maps] :
+       std::ranges::views::enumerate(std::ranges::views::zip(
+           m_non_zero_counter_map, m_maybe_start_group_map,
+           m_maybe_end_group_map))) {
+    auto const &[counters, start_groups, end_groups] = maps;
+    output << "Node[" << index << "]:\n";
+    output << "Counters: ";
+    for (auto const &counter : counters) {
+      output << counter << ", ";
     }
     output << "\n";
+    output << "Start Groups: ";
+    for (auto const &group : start_groups) {
+      output << group << ", ";
+    }
+    output << "\n";
+    output << "End Groups: ";
+    for (auto const &group : end_groups) {
+      output << group << ", ";
+    }
+    output << "\n\n";
   }
 }
--- src/private/graph_analyser.hpp
+++ src/private/graph_analyser.hpp
@@ -3,6 +3,7 @@
 #include "private/small_set.hpp"
 
 #include <cstddef>
+#include <cstdint>
 #include <ostream>
 #include <vector>
 
@@ -12,47 +13,30 @@ struct RegexGraphImpl;
 
 class GraphAnalyzer {
   size_t m_node_count;
-  std::vector<size_t> m_distance_map;
-  std::vector<SmallSet<size_t>> m_non_zero_counter_map;
-  std::vector<SmallSet<size_t>> m_maybe_start_group_map;
-  std::vector<SmallSet<size_t>> m_maybe_end_group_map;
+  std::vector<SmallSet<uint32_t>> m_non_zero_counter_map;
+  std::vector<SmallSet<uint32_t>> m_maybe_start_group_map;
+  std::vector<SmallSet<uint32_t>> m_maybe_end_group_map;
 
 public:
   GraphAnalyzer(RegexGraphImpl const &graph);
 
-  [[nodiscard]] constexpr auto distance(size_t to_node, size_t from_node)
-      -> size_t & {
-    return m_distance_map.at(from_node * m_node_count + to_node);
-  }
-
-  [[nodiscard]] constexpr auto distance(size_t to_node, size_t from_node) const
-      -> size_t const & {
-    return m_distance_map.at(from_node * m_node_count + to_node);
-  }
-
-  [[nodiscard]] constexpr auto are_connected(size_t to_node,
-                                             size_t from_node) const -> bool {
-    return distance(to_node, from_node) != ~0UL;
-  }
-
-  [[nodiscard]] constexpr auto get_non_zero_counters(size_t at_node) const
-      -> SmallSet<size_t> const & {
+  [[nodiscard]] constexpr auto
+  get_non_zero_counters(size_t at_node) const -> SmallSet<uint32_t> const & {
     return m_non_zero_counter_map[at_node];
   }
 
   [[nodiscard]] constexpr auto get_maybe_set_start_groups(size_t at_node) const
-      -> SmallSet<size_t> const & {
+      -> SmallSet<uint32_t> const & {
     return m_maybe_start_group_map[at_node];
   }
 
-  [[nodiscard]] constexpr auto get_maybe_set_end_groups(size_t at_node) const
-      -> SmallSet<size_t> const & {
+  [[nodiscard]] constexpr auto
+  get_maybe_set_end_groups(size_t at_node) const -> SmallSet<uint32_t> const & {
     return m_maybe_end_group_map[at_node];
   }
 
-  [[nodiscard]] constexpr auto is_group_start_unset(size_t at_node,
-                                                    size_t group) const
-      -> bool {
+  [[nodiscard]] constexpr auto
+  is_group_start_unset(size_t at_node, size_t group) const -> bool {
     return !get_maybe_set_start_groups(at_node).contains(group);
   }
 
